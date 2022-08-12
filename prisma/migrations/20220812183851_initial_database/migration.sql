10e3e949e8beee1b49a51a2ff13d3255a35744a5
Peter van Meijgaard <petervanmeijgaard@gmail.com>
2022-08-13 00:25:45 +0200

    Use PostgreSQL instead of SQLite

--- prisma/migrations/20220812183851_initial_database/migration.sql
+++
@@ -1,6 +0,0 @@
--- CreateTable
-CREATE TABLE "urls" (
-    "id" TEXT NOT NULL PRIMARY KEY,
-    "url" TEXT NOT NULL,
-    "short_url" TEXT NOT NULL
-);
---
+++ prisma/migrations/20220812222254_initial_database/migration.sql
@@ -0,0 +1,8 @@
+-- CreateTable
+CREATE TABLE "urls" (
+    "id" TEXT NOT NULL,
+    "url" TEXT NOT NULL,
+    "short_url" TEXT NOT NULL,
+
+    CONSTRAINT "urls_pkey" PRIMARY KEY ("id")
+);
