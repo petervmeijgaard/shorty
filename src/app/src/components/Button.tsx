77303bed8e7372937c5faf10394e1a4758108850
Peter van Meijgaard <petervanmeijgaard@gmail.com>
2022-08-12 20:37:24 +0200

    Add Button component

---
+++ src/app/src/components/Button.tsx
@@ -0,0 +1,14 @@
+import { ButtonHTMLAttributes, FC } from 'react';
+import cn from 'classnames';
+
+export const Button: FC<ButtonHTMLAttributes<HTMLButtonElement>> = ({ className, ...props }) => (
+  <button
+    className={cn(
+      'flex p-4 items-center rounded bg-slate-900 hover:bg-slate-800 transition-colors text-slate-50 disabled:hover:bg-slate-900 disabled:opacity-50 disabled:cursor-not-allowed',
+      className,
+    )}
+    {...props}
+  />
+);
+
+export default Button;
