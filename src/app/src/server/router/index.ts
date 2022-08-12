3216a6bdc47f219def4744a0124d0a350c83ffb4
Peter van Meijgaard <petervanmeijgaard@gmail.com>
2022-08-12 20:41:08 +0200

    Add zodError to response for tRPC

--- src/app/src/server/router/index.ts
+++ src/app/src/server/router/index.ts
@@ -1,11 +1,25 @@
 // src/server/router/index.ts
 import { createRouter } from './context';
 import superjson from 'superjson';
+import { ZodError } from 'zod';
 
 import { shortyRouter } from './shorty';
 
 export const appRouter = createRouter()
   .transformer(superjson)
+  .formatError(({ shape, error }) => {
+    return {
+      ...shape,
+      data: {
+        ...shape.data,
+        zodError:
+          error.code === 'BAD_REQUEST' &&
+          error.cause instanceof ZodError
+            ? error.cause.flatten()
+            : null,
+      },
+    };
+  })
   .merge('shorty.', shortyRouter);
 
 // export type definition of API
