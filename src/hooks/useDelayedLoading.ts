047c3ad376fa03d35f2669334a94cd9a99e61951
Peter van Meijgaard <petervanmeijgaard@gmail.com>
2022-08-14 16:56:51 +0200

    Disable ESLint warnings for now

--- src/hooks/useDelayedLoading.ts
+++ src/hooks/useDelayedLoading.ts
@@ -7,12 +7,14 @@ export const useDelayedLoading = (isLoading: boolean, delay: number = 500): bool
 
   const startTimer = useCallback(() => {
     timeout.start(() => setDerivedIsLoading(true), delay);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [delay, timeout.start]);
 
   const stopTimer = useCallback(() => {
     timeout.clear();
 
     setDerivedIsLoading(false);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [timeout.clear]);
 
   useEffect(() => {
