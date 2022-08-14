e6fea2cf42e4aab447bb6213f99e6604064e3867
Peter van Meijgaard <petervanmeijgaard@gmail.com>
2022-08-14 15:05:35 +0200

    Add Overlay component

---
+++ src/components/Overlay.tsx
@@ -0,0 +1,11 @@
+import cn from 'classnames';
+import { FC, HTMLAttributes } from 'react';
+
+export const Overlay: FC<HTMLAttributes<HTMLDivElement>> = ({ className, ...props }) => (
+  <div
+    className={cn('flex flex-1 inset-0 justify-center items-center fixed bg-slate-900/75 backdrop-blur-md', className)}
+    {...props}
+  />
+);
+
+export default Overlay;
