94cbab4c7f0d8ad0ce21318fa73bc022ff214482
Peter van Meijgaard <petervanmeijgaard@gmail.com>
2022-08-14 15:08:17 +0200

    Add overlay when mutation is loading

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,6 +7,8 @@ import SuccessNotification from '../components/SuccessNotification';
 import Button from '../components/Button';
 import TextInput from '../components/TextInput';
 import Form from '../components/Form';
+import LoadingIcon from '../components/LoadingIcon';
+import Overlay from '../components/Overlay';
 
 const AddLink: NextPage = () => {
   const [url, setUrl] = useState('');
@@ -25,36 +27,43 @@ const AddLink: NextPage = () => {
   const hasErrors = errors.length > 0;
 
   return (
-    <Card className="lg:w-1/2">
-      {shortenUrlMutation.data && (
-        <SuccessNotification>
-          Your shortened URL: {shortenUrlMutation.data}
-        </SuccessNotification>
+    <>
+      <Card className="lg:w-1/2">
+        {shortenUrlMutation.data && (
+          <SuccessNotification>
+            Your shortened URL: {shortenUrlMutation.data}
+          </SuccessNotification>
+        )}
+        {hasErrors && (
+          <ErrorNotification>
+            Whoops! Something went wrong &nbsp;
+            {errors.map(errorMessage => (
+              <span key={errorMessage}>{errorMessage}</span>
+            ))}
+          </ErrorNotification>
+        )}
+        <Form
+          className="sm:flex-row"
+          onSubmit={onSubmit}
+        >
+          <TextInput
+            type="url"
+            required
+            placeholder="Enter URL to short"
+            value={url}
+            onInput={event => setUrl(event.currentTarget.value)}
+          />
+          <Button disabled={shortenUrlMutation.isLoading}>
+            Shorten URL
+          </Button>
+        </Form>
+      </Card>
+      {shortenUrlMutation.isLoading && (
+        <Overlay>
+          <LoadingIcon className="text-7xl text-slate-50" />
+        </Overlay>
       )}
-      {hasErrors && (
-        <ErrorNotification>
-          Whoops! Something went wrong &nbsp;
-          {errors.map(errorMessage => (
-            <span key={errorMessage}>{errorMessage}</span>
-          ))}
-        </ErrorNotification>
-      )}
-      <Form
-        className="sm:flex-row"
-        onSubmit={onSubmit}
-      >
-        <TextInput
-          type="url"
-          required
-          placeholder="Enter URL to short"
-          value={url}
-          onInput={event => setUrl(event.currentTarget.value)}
-        />
-        <Button disabled={shortenUrlMutation.isLoading}>
-          Shorten URL
-        </Button>
-      </Form>
-    </Card>
+    </>
   );
 };
 
