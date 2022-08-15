3ac83a59ed0617f4d5399f61f75cc1d8cf40230f
Peter van Meijgaard <petervanmeijgaard@gmail.com>
2022-08-15 09:08:40 +0200

    Don't use the global environment

--- src/server/db/client.ts
+++ src/server/db/client.ts
@@ -1,16 +1,5 @@
 import { PrismaClient } from '@prisma/client';
-import { env } from '../../env/server.mjs';
 
-declare global {
-  var prisma: PrismaClient | undefined;
-}
-
-export const prisma =
-  global.prisma ||
-  new PrismaClient({
-    log: ['query'],
-  });
-
-if (env.NODE_ENV !== 'production') {
-  global.prisma = prisma;
-}
+export const prisma = new PrismaClient({
+  log: ['query'],
+});
