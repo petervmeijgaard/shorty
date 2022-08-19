b0d659bd9568a88e33b92d249a23af408cbcde8c
Peter van Meijgaard <petervanmeijgaard@gmail.com>
2022-08-19 15:42:01 +0200

    Rename a few variables

--- src/utils/shortenUrl.ts
+++ src/utils/shortenUrl.ts
@@ -1,13 +1,13 @@
 import { Buffer } from 'buffer';
-import baseX from 'base-x';
+import base from 'base-x';
 import md5 from 'md5';
 
 const BASE62 = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
 
 export const shortenUrl = (url: string) => {
-  const md5String = md5(url).slice(0, 7);
+  const md5Substring = md5(url).slice(0, 7);
 
-  return baseX(BASE62).encode(Buffer.from(md5String));
+  return base(BASE62).encode(Buffer.from(md5Substring));
 };
 
 export default shortenUrl;
