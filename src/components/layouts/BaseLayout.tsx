8d24dfa2de72ad9308c2af8876a8bb010e99d39b
Peter van Meijgaard <petervanmeijgaard@gmail.com>
2023-01-11 23:02:31 +0100

    Add top menu bar to BaseLayout

--- src/components/layouts/BaseLayout.tsx
+++ src/components/layouts/BaseLayout.tsx
@@ -1,20 +1,52 @@
+import { signOut, useSession } from 'next-auth/react';
 import Head from 'next/head';
+import Link from 'next/link';
 import { FC, PropsWithChildren } from 'react';
 
-const BaseLayout: FC<PropsWithChildren> = ({ children }) => (
-  <>
-    <Head>
-      <title>Shorty - Shorten your URL</title>
-      <meta
-        name="description"
-        content="A small and easy to use URL shortener"
-      />
-      <link rel="icon" href="/public/favicon.ico" />
-    </Head>
-    <div className="flex min-h-screen min-w-full items-center justify-center gap-4 bg-neutral-900">
-      <div className="container flex flex-1 justify-center p-4">{children}</div>
-    </div>
-  </>
-);
+const BaseLayout: FC<PropsWithChildren> = ({ children }) => {
+  const { data: session } = useSession();
+
+  return (
+    <>
+      <Head>
+        <title>Shorty - Shorten your URL</title>
+        <meta
+          name="description"
+          content="A small and easy to use URL shortener"
+        />
+        <link rel="icon" href="/public/favicon.ico" />
+      </Head>
+      <div className="flex min-h-screen min-w-full flex-col items-center justify-center bg-neutral-900">
+        <div className="container flex w-full items-center justify-between py-4">
+          <Link className="flex text-neutral-100" href="/">
+            Shorty
+          </Link>
+          <div className="flex gap-4">
+            {!session && (
+              <Link
+                className="rounded border border-neutral-100 py-2 px-3 text-neutral-100 transition hover:bg-neutral-100 hover:text-neutral-900"
+                href="/auth/sign-in"
+              >
+                Sign in
+              </Link>
+            )}
+
+            {session && (
+              <button
+                className="rounded border border-neutral-100 py-2 px-3 text-neutral-100 transition hover:bg-neutral-100 hover:text-neutral-900"
+                onClick={() => void signOut()}
+              >
+                Sign out
+              </button>
+            )}
+          </div>
+        </div>
+        <div className="container flex flex-1 items-center justify-center p-4">
+          {children}
+        </div>
+      </div>
+    </>
+  );
+};
 
 export default BaseLayout;
