626481c9b59398551cae14822ece5151cd01c05f
Peter van Meijgaard <petervanmeijgaard@gmail.com>
2023-01-11 22:16:10 +0100

    Refactor to Notification component

--- src/pages/500.tsx
+++ src/pages/500.tsx
@@ -1,7 +1,7 @@
 import { NextPage } from 'next';
 import Head from 'next/head';
 import Card from '@/components/ui/Card';
-import ErrorNotification from '@/components/ui/ErrorNotification';
+import Notification from '@/components/ui/Notification';
 
 export const InternalServerError: NextPage = () => (
   <>
@@ -9,14 +9,14 @@ export const InternalServerError: NextPage = () => (
       <title>500 - Internal server error</title>
     </Head>
     <Card className="lg:w-1/2">
-      <ErrorNotification className="flex-col">
+      <Notification.Error className="flex-col">
         <div className="flex flex-1 font-bold">
           Whoops! Something went wrong
         </div>
         <div className="flex flex-1">
           It looks like we need to fix something on our end...
         </div>
-      </ErrorNotification>
+      </Notification.Error>
     </Card>
   </>
 );
