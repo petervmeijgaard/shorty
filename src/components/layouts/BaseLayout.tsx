41494fbb026ee48b1213a541e4f3159209b61697
Peter van Meijgaard <petervanmeijgaard@gmail.com>
2023-01-12 23:25:02 +0100

    Add dropdown menu to BaseLayout

--- src/components/layouts/BaseLayout.tsx
+++ src/components/layouts/BaseLayout.tsx
@@ -1,10 +1,28 @@
 import { signOut, useSession } from 'next-auth/react';
 import Head from 'next/head';
 import Link from 'next/link';
-import { FC, PropsWithChildren } from 'react';
+import { useRouter } from 'next/router';
+import { FC, PropsWithChildren, useEffect, useRef } from 'react';
+import ArrowDownIcon from '~icons/ri/arrow-down-s-fill.jsx';
+import FadeTransition from '@/components/transitions/FadeTransition';
+import { useOnClickOutside } from '@/hooks/useOnClickOutside';
+import useVisibilityToggle from '@/hooks/useVisibilityToggle';
 
 const BaseLayout: FC<PropsWithChildren> = ({ children }) => {
+  const dropdownRef = useRef(null);
+  const dropdownMenu = useVisibilityToggle();
+  const router = useRouter();
   const { data: session } = useSession();
+
+  useOnClickOutside(dropdownRef, dropdownMenu.hide);
+
+  useEffect(() => {
+    router.events.on('routeChangeComplete', dropdownMenu.hide);
+
+    return () => {
+      router.events.off('routeChangeComplete', dropdownMenu.hide);
+    };
+  }, [dropdownMenu.hide, router.events]);
 
   return (
     <>
@@ -31,20 +49,32 @@ const BaseLayout: FC<PropsWithChildren> = ({ children }) => {
             )}
 
             {session && (
-              <>
-                <Link
-                  className="rounded border border-neutral-100 py-2 px-3 text-neutral-100 transition hover:bg-neutral-100 hover:text-neutral-900"
-                  href="/my-account"
-                >
-                  My Account
-                </Link>
+              <div className="relative" ref={dropdownRef}>
                 <button
-                  className="rounded border border-neutral-100 py-2 px-3 text-neutral-100 transition hover:bg-neutral-100 hover:text-neutral-900"
-                  onClick={() => void signOut()}
+                  className="flex flex-row items-center gap-2 rounded border border-neutral-100 py-2 px-3 text-neutral-100 transition hover:bg-neutral-100 hover:text-neutral-900"
+                  onClick={dropdownMenu.toggle}
                 >
-                  Sign out
+                  <span>Account</span>
+                  <ArrowDownIcon className="h-4 w-4" />
                 </button>
-              </>
+
+                <FadeTransition isVisible={dropdownMenu.isVisible}>
+                  <div className="absolute right-0 z-10 mt-4 flex w-56 flex-1 flex-col rounded border border-neutral-100 bg-neutral-900 py-1 text-neutral-100">
+                    <Link
+                      className="flex flex-1 py-2 px-3 text-neutral-100 transition hover:bg-neutral-100 hover:text-neutral-900"
+                      href="/my-account"
+                    >
+                      My Account
+                    </Link>
+                    <button
+                      className="flex flex-1 py-2 px-3 text-neutral-100 transition hover:bg-neutral-100 hover:text-neutral-900"
+                      onClick={() => void signOut()}
+                    >
+                      Sign out
+                    </button>
+                  </div>
+                </FadeTransition>
+              </div>
             )}
           </div>
         </div>
