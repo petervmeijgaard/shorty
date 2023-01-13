d49f637bfafae4aba7e43a8926ceeb0e44453e81
Peter van Meijgaard <petervanmeijgaard@gmail.com>
2023-01-13 12:05:43 +0100

    Improve MyUrls section

--- src/components/pages/my-account/MyUrls.tsx
+++ src/components/pages/my-account/MyUrls.tsx
@@ -1,69 +1,47 @@
 import cn from 'classnames';
 import Link from 'next/link';
-import { ComponentPropsWithoutRef, FC, memo } from 'react';
+import { ComponentPropsWithoutRef, FC, memo, useMemo } from 'react';
 import Shimmer from '@/components/ui/Shimmer';
 import TableBodyCell from '@/components/ui/TableBodyCell';
 import TableHeaderCell from '@/components/ui/TableHeaderCell';
+import { GetElementType } from '@/types/helpers';
 import { api, RouterOutputs } from '@/utils/api';
 
-type SuccessStateProps = {
-  data: RouterOutputs['shorty']['myUrls'];
-};
+type DataTableRowProps = GetElementType<RouterOutputs['shorty']['myUrls']>;
 
-const SuccessState: FC<SuccessStateProps> = ({ data }) => {
-  if (!data.length) {
-    return (
-      <tr>
-        <TableBodyCell colSpan={3}>No URLs shortened yet</TableBodyCell>
-      </tr>
-    );
-  }
-
-  return (
-    <>
-      {data.map(url => (
-        <tr key={url.id}>
-          <TableBodyCell>{url.url}</TableBodyCell>
-          <TableBodyCell>
-            <Link href={`/${url.shortUrl}`} target="_blank">
-              /{url.shortUrl}
-            </Link>
-          </TableBodyCell>
-          <TableBodyCell>{url.hits}</TableBodyCell>
-        </tr>
-      ))}
-    </>
-  );
-};
-
-const LoadingState: FC = () => (
-  <>
-    <tr>
-      <TableBodyCell>
-        <Shimmer className="h-6 w-[250px]" />
-      </TableBodyCell>
-      <TableBodyCell>
-        <Shimmer className="h-6 w-[100px]" />
-      </TableBodyCell>
-      <TableBodyCell>
-        <Shimmer className="h-6 w-[30px]" />
-      </TableBodyCell>
-    </tr>
-    <tr>
-      <TableBodyCell>
-        <Shimmer className="h-6 w-[250px]" />
-      </TableBodyCell>
-      <TableBodyCell>
-        <Shimmer className="h-6 w-[100px]" />
-      </TableBodyCell>
-      <TableBodyCell>
-        <Shimmer className="h-6 w-[30px]" />
-      </TableBodyCell>
-    </tr>
-  </>
+const DataTableRow: FC<DataTableRowProps> = ({ url, shortUrl, hits }) => (
+  <tr>
+    <TableBodyCell>{url}</TableBodyCell>
+    <TableBodyCell>
+      <Link href={`/${shortUrl}`} target="_blank">
+        /{shortUrl}
+      </Link>
+    </TableBodyCell>
+    <TableBodyCell>{hits}</TableBodyCell>
+  </tr>
 );
 
-const ErrorState = () => (
+const NotFoundTableRow: FC = () => (
+  <tr>
+    <TableBodyCell colSpan={3}>No URLs shortened yet</TableBodyCell>
+  </tr>
+);
+
+const LoadingTableRow: FC = () => (
+  <tr>
+    <TableBodyCell>
+      <Shimmer className="h-6 w-[250px]" />
+    </TableBodyCell>
+    <TableBodyCell>
+      <Shimmer className="h-6 w-[100px]" />
+    </TableBodyCell>
+    <TableBodyCell>
+      <Shimmer className="h-6 w-[30px]" />
+    </TableBodyCell>
+  </tr>
+);
+
+const ErrorTableRow: FC = () => (
   <tr>
     <TableBodyCell colSpan={3}>
       Something went wrong and loading the URLs failed.
@@ -77,6 +55,28 @@ const MyUrls: FC<ComponentPropsWithoutRef<'div'>> = ({
 }) => {
   const query = api.shorty.myUrls.useQuery();
 
+  const tableBody = useMemo(() => {
+    if (query.isLoading) {
+      return (
+        <>
+          <LoadingTableRow />
+          <LoadingTableRow />
+          <LoadingTableRow />
+        </>
+      );
+    }
+
+    if (query.isError) {
+      return <ErrorTableRow />;
+    }
+
+    if (!query.data.length) {
+      return <NotFoundTableRow />;
+    }
+
+    return query.data.map(url => <DataTableRow key={url.id} {...url} />);
+  }, [query]);
+
   return (
     <div className={cn('flex flex-col gap-4', className)} {...props}>
       <h2 className="text-2xl font-light text-neutral-900">My Shorty URLs</h2>
@@ -88,11 +88,7 @@ const MyUrls: FC<ComponentPropsWithoutRef<'div'>> = ({
             <TableHeaderCell>Hits</TableHeaderCell>
           </tr>
         </thead>
-        <tbody>
-          {query.isLoading && <LoadingState />}
-          {query.isError && <ErrorState />}
-          {query.isSuccess && <SuccessState data={query.data} />}
-        </tbody>
+        <tbody>{tableBody}</tbody>
       </table>
     </div>
   );
